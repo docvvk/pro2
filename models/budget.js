dca29c80e59eaf31549cd7f4b04f82b2986aed03
Vivek Malhan <vivek.malhan@yahoo.com>
2018-09-11 15:56:11 -0400

    updated html

--- models/budget.js
+++ models/budget.js
@@ -3,13 +3,13 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection.js");
 
 var Transaction = sequelize.define("transaction", {
-  author: Sequelize.STRING,
+  name: Sequelize.STRING,
   type: Sequelize.STRING,
   from: Sequelize.STRING,
   amount: Sequelize.STRING,
   category: Sequelize.STRING,
-  notes: Sequelize.STRING,
-  created_at: Sequelize.DATE
+  notes: Sequelize.STRING
+  // created_at: Sequelize.DATE
 });
 
 Transaction.sync();
